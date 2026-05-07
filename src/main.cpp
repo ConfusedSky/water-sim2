d493afa8a5e8d709279d7d83d8d10065332f8882
Masa Maeda <masamaedae@gmail.com>
2026-05-07 16:55:43 -0700

    Back to standard world settings

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ struct FrameTimingWindow {
 };
 
 constexpr float kFixedDt     = 1.0f / 120.0f;
-constexpr int   kMaxSubsteps = 1;
+constexpr int   kMaxSubsteps = 4;
 constexpr int   kMouseButtonNone = 0;
 constexpr int   kMouseButtonPush = 2;
 
